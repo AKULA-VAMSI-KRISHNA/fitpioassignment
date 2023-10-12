be0b90180ba85fac57e6ed9b08fd35f91d9b8e88
AKULA-VAMSI-KRISHNA <vamsikrishnaakula2@gmail.com>
2023-10-13 02:53:35 +0530

    first

---
+++ src/Home.js
@@ -0,0 +1,162 @@
+import React from 'react'
+import "./Home.css";
+function home() {
+  return (
+    <div className='all'>
+      <div className='all1'>
+        <h3>Dashboard</h3>
+        <p>Product &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;>&nbsp;&nbsp;</p>
+        <p>Customers&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;>&nbsp;&nbsp;</p>
+        <p>Income&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;>&nbsp;&nbsp;</p>
+        <p>Promote&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;>&nbsp;&nbsp;</p>
+        <p>Help&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;>&nbsp;&nbsp;</p>
+        <div className='all11'>
+             <img src="https://png.pngtree.com/png-vector/20190223/ourmid/pngtree-profile-glyph-black-icon-png-image_691589.jpg" alt="" width="15%" height="15%"/>
+             <div className='all111'>
+                <h4>Evaro</h4>
+                <p>Project Manager</p>
+             </div>
+        </div>
+      </div>
+      <div className='all2'>
+        <div className='all21'>
+        <h3>helllo Sharukh <img src="https://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/waving-hand.png" alt="" width="2%" /></h3>
+        <div className='all211'>
+            <input type="text" placeholder='search' style={{border:"none" ,padding:"5%", marginTop:"10%" , borderRadius:"5px"}}/>
+        </div>
+        </div>
+        <div className='all22'>
+            <div className='all221'>
+                <div>
+                    <img src="https://www.freeiconspng.com/thumbs/dollar-icon-png/dollar-round-icon--18.png" alt="" width="70%" />
+                </div>
+                <div>
+                    <p style={{color:"#B0B0B0"}}>Earning</p>
+                    <h2>$198k</h2>
+                    <p><span style={{color:"green"}}>+37.8%</span>this month</p>
+                </div>  
+            </div>
+            <div className='all222'>
+                <div>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/9/9c/Forms.png" alt="" width="70%" />
+                </div>
+                <div>
+                    <p style={{color:"#B0B0B0"}}>Orders</p>
+                    <h2>$2.4k</h2>
+                    <p><span style={{color:"red"}}>-2%</span>this month</p>
+                </div>  
+            </div>
+            <div className='all223'>
+                <div>
+                    <img src="https://cdn-icons-png.flaticon.com/512/6975/6975721.png" alt="" width="70%" />
+                </div>
+                <div>
+                    <p style={{color:"#B0B0B0"}}>Balance</p>
+                    <h2>$2.4k</h2>
+                    <p><span style={{color:"red"}}>-2%</span>this month</p>
+                </div>  
+            </div>
+            <div className='all224'>
+                <div>
+                    <img src="https://media.istockphoto.com/id/1276423485/vector/shopping-bag-design-icon-shopping-logo-shop-concept-vector-illustration.jpg?s=1024x1024&w=is&k=20&c=fyh_ioAF73UbFFEEKGvNsLCsRgEVktIbohqigWrEbEM=" alt="" width="70%" />
+                </div>
+                <div>
+                    <p style={{color:"#B0B0B0"}}>Sales</p>
+                    <h2>$11k</h2>
+                    <p><span style={{color:"green"}}>+11%</span>this month</p>
+                </div>  
+            </div>
+        </div>
+        <div className='all23'>
+            <div className='all231'>
+            <div>
+                <h3>Overview</h3>
+                <p style={{color:"#B0B0B0"}}>monthly earning</p>
+            </div>
+            <div style={{display:"grid" , gridTemplateColumns:"1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr"}}>
+            <div style={{display:"flex" , flexDirection:"column" ,padding:"1% 1%"}}>
+                <input type="text" disabled style={{border:"none" , backgroundColor:"#B3DBED",height:"130px",width:"30px",borderRadius:"10px", marginTop:"50px"}}/>
+                <p>jan</p>
+            </div>
+            <div style={{display:"flex" , flexDirection:"column" ,padding:"1% 1%"}}>
+                <input type="text" disabled style={{border:"none" , backgroundColor:"#B3DBED",height:"110px",width:"30px",borderRadius:"10px", marginTop:"70px"}}/>
+                <p>feb</p>
+            </div>
+            <div style={{display:"flex" , flexDirection:"column" ,padding:"1% 1%"}}>
+                <input type="text" disabled style={{border:"none" , backgroundColor:"#B3DBED",height:"170px",width:"30px",borderRadius:"10px", marginTop:"10px"}}/>
+                <p>mar</p>
+            </div>
+            <div style={{display:"flex" , flexDirection:"column" ,padding:"1% 1%"}}>
+                <input type="text" disabled style={{border:"none" , backgroundColor:"#B3DBED",height:"160px",width:"30px",borderRadius:"10px", marginTop:"20px"}}/>
+                <p>apr</p>
+            </div>
+            <div style={{display:"flex" , flexDirection:"column" ,padding:"1% 1%"}}>
+                <input type="text" disabled style={{border:"none" , backgroundColor:"#B3DBED",height:"100px",width:"30px",borderRadius:"10px", marginTop:"80px"}}/>
+                <p>may</p>
+            </div>
+            <div style={{display:"flex" , flexDirection:"column" ,padding:"1% 1%"}}>
+                <input type="text" disabled style={{border:"none" , backgroundColor:"#B3DBED",height:"130px",width:"30px",borderRadius:"10px", marginTop:"50px"}}/>
+                <p>jun</p>
+            </div>
+            <div style={{display:"flex" , flexDirection:"column" ,padding:"1% 1%"}}>
+                <input type="text" disabled style={{border:"none" , backgroundColor:"#B3DBED",height:"80px",width:"30px",borderRadius:"10px", marginTop:"100px"}}/>
+                <p>jul</p>
+            </div>
+            <div style={{display:"flex" , flexDirection:"column" ,padding:"1% 1%"}}>
+                <input type="text" disabled style={{border:"none" , backgroundColor:"#4DB2E0",height:"180px",width:"30px",borderRadius:"10px", marginTop:"0px"}}/>
+                <p>aug</p>
+            </div>
+            <div style={{display:"flex" , flexDirection:"column" ,padding:"1% 1%"}}>
+                <input type="text" disabled style={{border:"none" , backgroundColor:"#B3DBED",height:"135px",width:"30px",borderRadius:"10px", marginTop:"45px"}}/>
+                <p>sep</p>
+            </div><div style={{display:"flex" , flexDirection:"column" ,padding:"1% 1%"}}>
+                <input type="text" disabled style={{border:"none" , backgroundColor:"#B3DBED",height:"130px",width:"30px",borderRadius:"10px", marginTop:"50px"}}/>
+                <p>oct</p>
+            </div>
+            <div style={{display:"flex" , flexDirection:"column" ,padding:"1% 1%"}}>
+                <input type="text" disabled style={{border:"none" , backgroundColor:"#B3DBED",height:"150px",width:"30px",borderRadius:"10px", marginTop:"30px"}}/>
+                <p>nov</p>
+            </div>
+            <div style={{display:"flex" , flexDirection:"column" ,padding:"1% 1%"}}>
+                <input type="text" disabled style={{border:"none" , backgroundColor:"#B3DBED",height:"145px",width:"30px",borderRadius:"10px", marginTop:"35px"}}/>
+                <p>dec</p>
+            </div>
+            </div>
+            </div>
+            <div className='all232'>
+                <h3>Customers</h3>
+                <p style={{color:"#B0B0B0"}}>Customers that buy products</p>
+                <img src="./image.png" alt="" />
+            </div>
+        </div>
+        <div className='all24'>
+            <div style={{display:"grid", gridTemplateColumns:"6fr 2fr 2fr"}}>
+                <h3>Product shell</h3>
+                <input type="text" placeholder='search' style={{border:"none"}} />
+                <input type="text" placeholder='last 30days' style={{border:"none"}}/>
+            </div>
+            <div style={{display:"grid" , gridTemplateColumns:"6fr 1fr 1fr 1fr" ,borderBottom:"1px solid #b0b0b0"}}>
+                <p style={{color:"#B0B0B0"}}>Product Name</p>
+                <p style={{color:"#B0B0B0"}}>Stock</p>
+                <p style={{color:"#B0B0B0"}}>Price</p>
+                <p style={{color:"#B0B0B0"}}>Total Sales</p>
+            </div>
+            <div style={{display:"grid" ,gridTemplateColumns:"6fr 1fr 1fr 1fr"}}>
+                <h4>Abstract 3D </h4>
+                <p>32 instock</p>
+                <h5>$45.99</h5>
+                <p>20</p>
+            </div>
+            <div style={{display:"grid" ,gridTemplateColumns:"6fr 1fr 1fr 1fr"}}>
+                <h4>sarphens illustration</h4>
+                <p>32 instock</p>
+                <h5>$45.99</h5>
+                <p>20</p>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default home
